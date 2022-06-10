30c46411b3d26635cdfdb2c18468b4db8fc4fda3
Kunal Keshan <kunalkeshan12@gmail.com>
2022-06-10 19:31:22 +0530

    update login page

--- Day-2/insta-clone/frontend/src/App.jsx
+++ Day-2/insta-clone/frontend/src/App.jsx
@@ -1,14 +1,17 @@
-
 import './App.css';
 import Login from "./login/login"
 import Main from './main/main';
 import Navbar from './navbar/navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <div className='app'>
-      <Navbar/>
-      {/* <Login/> */}
-      <Main/>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<><Navbar /><Main /></>} />
+          <Route path='/auth' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- Day-2/insta-clone/frontend/src/login/login.jsx
+++ Day-2/insta-clone/frontend/src/login/login.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import './login.css'
 
 const Login = () => {
+  const [page, setPage] = useState('login');
+
+  const switchPage = () => {
+    setPage(page === 'login' ? 'signup' : 'login');
+  }
+
+  const Login = () => {
+    return (
+      <div className='container'>
+        <div className='box'>
+          <div className='logo'>Petstagram</div>
+          <form id='login-form'>
+            <input type='text' />
+            <input type='password' />
+            <button type='submit'>Login</button>
+          </form>
+          <div href='/auth' className='link'>Forgot Password?</div>
+        </div>
+        <div className='box'>
+          <p className=''>Don't have an account? <span className='navigate' onClick={switchPage}>Sign up</span></p>
+        </div>
+      </div>
+    )
+  };
+
+  const Signup = () => {
+    return (
+      <div className='container'>
+        <div className='box'>
+          <div className='logo'>Petstagram</div>
+          <p>Sign up to see photos and videos from your friends.</p>
+          <form id='signup-form'>
+            <input type='text' />
+            <input type='text' />
+            <input type='text' />
+            <input type='text' />
+            <button type='submit'>Sign up</button>
+          </form>
+        </div>
+        <div className='box'>
+          <p className=''>Have an account? <span className='navigate' onClick={switchPage}>Login</span></p>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div className='abc'>Login</div>)
+    <div className='login-wrapper'>
+      {
+        page === 'login' ? <Login /> : <Signup />
+      }
+    </div>)
 }
 
 export default Login
